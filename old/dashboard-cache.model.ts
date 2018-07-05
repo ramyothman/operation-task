e1abe8d0faa3d67c61b5e9bd11f42e356c7ff7ee
Yahya Alaa <yahyaalaamassoud@gmail.com>
2018-07-05 19:10:19 +0200

    Cache model

---
+++ old/dashboard-cache.model.ts
@@ -0,0 +1,23 @@
+import * as _ from "lodash";
+
+export class Cache {
+    
+    private static instance: Cache;
+    private cache: any;
+
+    private Cache() {}
+
+    public static get Instance(): Cache {
+        return this.instance || (this.instance = new this());
+    }
+
+    public getCache(index: string, obj: any[], field: string) {
+        if (!this.cache[index])
+            this.cache[index] = _.sumBy(obj, field);
+        return this.cache[index];
+    }
+
+    public resetCache() {
+        this.cache = [];
+    }
+}
